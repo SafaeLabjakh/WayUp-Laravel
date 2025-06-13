82c34008ba71c7ff9a57af1eab41670acc7ac959
safae <labjakhsafae160@gmail.com>
2025-06-13 13:53:48 +0100

    migration

--- backend/database/migrations/0001_01_01_000000_create_users_table.php
+++ backend/database/migrations/0001_01_01_000000_create_users_table.php
@@ -12,12 +12,13 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('users', function (Blueprint $table) {
-            $table->id();
-            $table->string('name');
+             $table->id(); 
+            $table->string('first_name');
+            $table->string('last_name');
+            $table->string('metier_sugg')->nullable(); 
             $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
-            $table->rememberToken();
+            $table->boolean('test_done')->default(false);
             $table->timestamps();
         });
 
