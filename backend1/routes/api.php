f0c098f07e9b2c49121e7c13e915b62320a66ff0
safae <labjakhsafae160@gmail.com>
2025-06-14 12:54:43 +0100

    get & update skills

--- backend1/routes/api.php
+++ backend1/routes/api.php
@@ -3,8 +3,11 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\FormationController;
 use App\Http\Controllers\MetierController; 
+use App\Http\Controllers\SkillController;
 
 
 Route::post('/getFormations', [FormationController::class, 'getFormations']);
 Route::get('/formations/{userId}', [FormationController::class, 'getUserFormations']);
 Route::post('/resultat/{userId}', [MetierController::class, 'generateCareer']);
+Route::get('/skills/{userId}', [SkillController::class, 'getSkillsByUserId']);
+Route::put('/skills/{skillId}', [SkillController::class, 'updateSkill']);
